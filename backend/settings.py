3c68e20a773aa262039f2203f14f3635a18118c5
mkrolak <42621308+theRealMkrolak@users.noreply.github.com>
2026-01-05 18:35:34 -0600

    feat: fixed settings file

--- backend/settings.py
+++ backend/settings.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    system: Literal['raspberrypi', 'desktop'] = 'raspberrypi'
+    system: Literal["raspberrypi", "mac"] = "raspberrypi"
     raspap_api_key: str | None = None
-    mediamtx_hls_url: str = 'http://localhost:8888/mystream'
+    mediamtx_hls_url: str = "http://localhost:8888/mystream"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
